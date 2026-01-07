a009468143d70dd6ef76a8a7ba8a4c5fff2d789b
Vyacheslav Kuznetsov <vyacheslav.kuznetsov@veeam.com>
2026-01-07 19:18:44 +0100

    wip

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,7 +3,6 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
 
-
 export function mergeUrlParts(baseUrl: string, suffix: string): string {
   const trimmedBaseUrl = baseUrl.replace(/\/+$/, '');
   const trimmedSuffix = suffix.replace(/^\/+/, '');
